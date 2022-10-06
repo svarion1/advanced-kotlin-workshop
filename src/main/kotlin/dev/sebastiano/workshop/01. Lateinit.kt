9108f21a21f8ff1a2065038d8cfbae29d455a567
svarion1 <alessandro.scisci@edu.itspiemonte.it>
2022-10-06 09:31:22 +0200

    completed first excercise

--- src/main/kotlin/dev/sebastiano/workshop/01. Lateinit.kt
+++ src/main/kotlin/dev/sebastiano/workshop/01. Lateinit.kt
@@ -5,8 +5,8 @@ internal class LateInit {
     private lateinit var _fruit: String
 
     var fruit: String?
-        get() = TODO()
-        set(value) = TODO()
+        get() = if (::_fruit.isInitialized) _fruit else null
+        set(value) = if (value != null) _fruit = value else throw IllegalArgumentException("Fruit cannot be null")
 
     fun performSomeWork(): String = "Here's something for ya: $_fruit"
 }
